86028efd201fda4ae0fcc7b9fdf3dd6332647cdd
ilee304 <Isaiah.Lee@quinnipiac.edu>
2015-11-04 16:40:53 +0000

    Setup Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn final_app.wsgi --log-file -
--- final_app/settings.py
+++ final_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+ON_HEROKU = os.getenv('ON_HEROKU', False)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -23,7 +23,10 @@ MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'jf*b&3sw#b2fypj3o%s_%v3&fi$b@dl&eo6rvz)=@a!=u3g&*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ON_HEROKU == True:
+  DEBUG = False
+else:
+  DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -74,13 +77,19 @@ WSGI_APPLICATION = 'final_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if ON_HEROKU == False:
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      }
+  }
+else:
+  DATABASES = {}
+  import dj_database_url
+  DATABASES['default'] =  dj_database_url.config()
+  SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+  ALLOWED_HOSTS = ['*']
 
 
 # Internationalization
@@ -102,3 +111,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
+STATIC_ROOT = 'staticfiles'
